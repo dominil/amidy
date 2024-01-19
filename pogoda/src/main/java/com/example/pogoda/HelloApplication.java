84e81e5f0310f02c73751d79b5ecc17bb2d9c995
dominil <104647138+dominil@users.noreply.github.com>
2024-01-19 14:07:09 +0100

    Add files via upload

--- pogoda/src/main/java/com/example/pogoda/HelloApplication.java
+++ pogoda/src/main/java/com/example/pogoda/HelloApplication.java
@@ -40,7 +40,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         String link_1= "https://api.openweathermap.org/data/2.5/weather?q=";
-        String link_2="&appid=dbf2677cfd923964172672906d1b0f03";
+        String link_2="&appid=dbf2677cfd923964172672906d1b0f03&lang=pl";
 
         Image lupa_zdjecie= new Image("C:\\Users\\Użytkownik\\IdeaProjects\\pogoda\\zdjecia\\lupa.jpg");
         ImageView lupa= new ImageView(lupa_zdjecie);
@@ -51,7 +51,7 @@ public class HelloApplication extends Application {
         Button button_wyszukaj= new Button();
 
         wartosci.setFill(Color.RED);
-        wartosci.setFont(Font.font("Verdana", FontWeight.BOLD, 40));
+        wartosci.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
 
         api_weather.setFill(Color.BLUE);
         api_weather.setFont(new Font(40));
@@ -81,27 +81,64 @@ public class HelloApplication extends Application {
             public void handle(MouseEvent mouseEvent) {
                 String nazwa_miasta=pole_do_wypelnienia.getText().toString();
                 try {
+                    //połączenie JSON
                     URL url_api= new URL(link_1+nazwa_miasta+link_2);
                     HttpURLConnection connection= (HttpURLConnection) url_api.openConnection();
                     connection.setRequestMethod("GET");
-
                     BufferedReader reader= new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
                     String dane=reader.readLine();
-
                     JSONObject jsonObject= new JSONObject(dane);
-
                     String dane_do_pokazania="";
+
+
+
 
                     dane_do_pokazania=dane_do_pokazania+jsonObject.get("name")+"\n";
 
+
+                    //temperatura
                     String temp ="";
+                    String min_temp="";
+                    String max_temp="";
+                    String feels_temp="";
+                    String cisnienie="";
+                    String wiatr="";
+                    String zachmurzenie="";
+
                     temp +=jsonObject.getJSONObject("main").get("temp");
+                    min_temp +=jsonObject.getJSONObject("main").get("temp_min");
+                    max_temp +=jsonObject.getJSONObject("main").get("temp_max");
+                    feels_temp +=jsonObject.getJSONObject("main").get("feels_like");
+                    cisnienie +=jsonObject.getJSONObject("main").get("pressure");
+                    wiatr +=jsonObject.getJSONObject("wind").get("speed");
+                    zachmurzenie +=jsonObject.getJSONObject("clouds").get("all");
+
+
                     float tmp = parseFloat(temp);
                     tmp = tmp - 273;
                     tmp= (float) Math.round(tmp*10)/10;
-                    dane_do_pokazania+="Temperatura: "+tmp+" °C \n";
 
+                    float tmp_min = parseFloat(min_temp);
+                    tmp_min -=273;
+                    tmp_min= (float) Math.round(tmp_min*10)/10;
+
+                    float tmp_max = parseFloat(max_temp);
+                    tmp_max -=273;
+                    tmp_max= (float) Math.round(tmp_max*10)/10;
+
+                    float tmp_feels = parseFloat(feels_temp);
+                    tmp_feels -=273;
+                    tmp_feels= (float) Math.round(tmp_feels*10)/10;
+
+                    dane_do_pokazania+="Temperatura: "+tmp+" °C \n";
+                    dane_do_pokazania+="Temperatura min: "+tmp_min+" °C \n";
+                    dane_do_pokazania+="Temperatura max: "+tmp_max+" °C \n";
+                    dane_do_pokazania+="Temperatura odczuwalna: "+tmp_feels+" °C \n";
+                    dane_do_pokazania+="Cisnienie: "+cisnienie+" hPa\n";
+                    dane_do_pokazania+="Predkosc wiatru: "+wiatr+" Km/H\n";
+                    dane_do_pokazania+="Zachmurzenei: "+zachmurzenie+" %\n";
+
+                    //
 
                     wartosci.setText(dane_do_pokazania);
 
@@ -129,7 +166,7 @@ public class HelloApplication extends Application {
 
         Group group = new Group(gridPane);
         Scene scene= new Scene(group,700,500);
-        scene.setFill(Color.BEIGE);
+        scene.setFill(Color.BLACK);
 
         stage.setTitle("Pogoda");
         stage.setScene(scene);
